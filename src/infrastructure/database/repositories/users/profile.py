1ebdf158dca3d04baadd3644cf4e176423745403
Ivan Fedorov <ivan45sc@gmail.com>
2026-02-10 00:43:16 +0500

    add profile repository

---
+++ src/infrastructure/database/repositories/users/profile.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.users.profile.entities import ProfileCreationDTO
+from infrastructure.database.models.profile import ProfileModel
+
+
+class ProfileRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def add(self, data: ProfileCreationDTO, user_id: UUID) -> None:
+        self.session.add(ProfileModel(
+            user_id=user_id,
+            first_name=data.first_name,
+            last_name=data.last_name,
+            birth_date=data.birth_date,
+            gender=data.gender
+        ))
+
+    async def get_by_user_id(self, user_id: UUID) -> ProfileModel:
+        stmt = select(ProfileModel).where(ProfileModel.user_id == user_id)
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
+
+    async def get_by_id(self, profile_id: UUID) -> ProfileModel:
+        stmt = select(ProfileModel).where(ProfileModel.id == profile_id)
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
