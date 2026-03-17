fff4267f64be1a4ddbd11089cd9f1b7928804dd6
Ivan Fedorov <ivan45sc@gmail.com>
2026-03-17 11:11:01 +0500

    add celery and redis services in docker compose

---
+++ src/infrastructure/celery_app/worker.py
@@ -0,0 +1,14 @@
+from celery import Celery
+
+from settings import settings
+
+
+
+celery = Celery(
+    "worker",
+    broker=settings.redis.celery_url,
+    backend=settings.redis.celery_url,
+    include=[
+        "core.users.auth.tasks"
+    ]
+)
--- src/settings.py
+++ src/settings.py
@@ -9,6 +9,12 @@ BaseSettings.model_config = SettingsConfigDict(
 )
 
 
+class AppSettings(BaseSettings):
+    base_url: str
+
+    model_config = SettingsConfigDict(env_prefix="APP_")
+
+
 class DatabaseSettings(BaseSettings):
     host: str
     port: int
@@ -17,7 +23,7 @@ class DatabaseSettings(BaseSettings):
     db: str
 
     @property
-    def database_url(self):
+    def database_url(self) -> str:
         return f"postgresql+asyncpg://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.db}"
 
     model_config = SettingsConfigDict(
@@ -39,10 +45,36 @@ class S3Settings(BaseSettings):
     public_endpoint: str = Field(alias="PUBLIC_ENDPOINT_URL")
 
 
+class RedisSettings(BaseSettings):
+    host: str
+    port: int
+
+    celery_db: int
+
+    @property
+    def celery_url(self) -> str:
+        return f"redis://{self.host}:{self.port}/{self.celery_db}"
+
+    model_config = SettingsConfigDict(env_prefix="REDIS_")
+
+
+class EmailSettings(BaseSettings):
+    host: str
+    port: int
+    name: str
+    password: SecretStr
+    from_email: str
+
+    model_config = SettingsConfigDict(env_prefix="EMAIL_")
+
+
 class Settings(BaseSettings):
+    app: AppSettings = Field(default_factory=AppSettings)
     db: DatabaseSettings = Field(default_factory=DatabaseSettings)
     security: SecuritySettings = Field(default_factory=SecuritySettings)
     s3: S3Settings = Field(default_factory=S3Settings)
+    redis: RedisSettings = Field(default_factory=RedisSettings)
+    email: EmailSettings = Field(default_factory=EmailSettings)
 
 
 settings = Settings()
